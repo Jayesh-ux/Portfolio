49594732abb2435952bb697ddebfe7740eaf69ea
Jayesh-ux <hsinghjayesh@gmail.com>
2024-02-02 11:21:58 -0800

    added db conf to fetch

--- frontend/components/recent-works-container.js
+++ frontend/components/recent-works-container.js
@@ -14,7 +14,7 @@ const RecentWorksContainer = () => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://127.0.0.1:8000/');
+      const response = await axios.get('https://portfolio-2jzqzbf2p-jayesh-singhs-projects.vercel.app/');
       setData(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);
