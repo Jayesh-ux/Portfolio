254d320a79efc9e28818eacb9dd9567854b3cdc3
Jayesh-ux <hsinghjayesh@gmail.com>
2024-02-02 07:01:53 -0800

    static  conf

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,7 +1,7 @@
 pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
+
 
 echo  "make Migrations"
 python3.9 manage.py makemigration --noinput
 python3.9 manage.py migrate --noinput
-
-python3.9 manage.py collectstatic --noinput
