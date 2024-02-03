f6dc0aabe1971411a5a70fee827c3e8cf37e091d
Jayesh-ux <hsinghjayesh@gmail.com>
2024-02-02 21:29:31 -0800

    cors policy issue trying to fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -148,14 +148,3 @@ CORS_ALLOWED_ORIGINS = [
 
 # Optional: Allow all headers and methods.
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
